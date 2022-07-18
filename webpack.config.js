6c63af1150b3b01b64655c8a8dc74f6d5a3e8350
Juan Camilo Rodriguez Giraldo <juanrodriguez1028@gmail.com>
2022-07-17 23:13:22 -0500

    primer commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MinicssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /styles\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /styles\.css$/,
+        use: [MinicssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new MinicssExtractPlugin({
+      filename: "[name].css",
+      ignoreOrder: false,
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets/" }],
+    }),
+  ],
+};
